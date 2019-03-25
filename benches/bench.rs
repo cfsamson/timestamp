ad44e23968bf7a41759db36336c2052a8c426af9
cfsamson <cf@samson.no>
2019-03-25 13:45:58 +0100

    Added more benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -53,3 +53,93 @@ fn chrono_timestamp(b: &mut Bencher) {
         test::black_box(timestamps);
     })
 }
+
+#[bench]
+fn naive_timestamp_create(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for i in 0..30 {
+                let ts = TimeStamp2::new(19, 1, 1 + i, 1);
+                &timestamps.push(ts);
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
+
+#[bench]
+fn custom_timestamp_create(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for i in 0..30 {
+                let ts = TimeStamp::new(19, 1, 1 + i, 1);
+                &timestamps.push(ts);
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
+
+#[bench]
+fn chrono_timestamp_create(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for i in 0..30 {
+                let ts = TimeStampChrono::new(19, 1, 1 + i, 1);
+                &timestamps.push(ts);
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
+
+#[bench]
+fn naive_timestamp_read(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let tsr: Vec<TimeStamp2> = (0..30).map(|i| TimeStamp2::new(19, 1, 1 + i, 1)).collect();
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for ts in &tsr {
+                &timestamps.push(ts.as_ymdh());
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
+
+#[bench]
+fn custom_timestamp_read(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let tsr: Vec<TimeStamp> = (0..30).map(|i| TimeStamp::new(19, 1, 1 + i, 1)).collect();
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for ts in &tsr {
+                &timestamps.push(ts.as_ymdh());
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
+
+#[bench]
+fn chrono_timestamp_read(b: &mut Bencher) {
+    
+    b.iter(|| {
+        let tsr: Vec<TimeStampChrono> = (0..30).map(|i| TimeStampChrono::new(19, 1, 1 + i, 1)).collect();
+        let mut timestamps = vec![];
+        for _ in 0..20 {
+            for ts in &tsr {
+                &timestamps.push(ts.as_ymdh());
+            }
+        }
+        test::black_box(timestamps);
+    })
+}
